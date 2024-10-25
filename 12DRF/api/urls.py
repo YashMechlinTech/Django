9576c3a74c683501902cc6d2ea14d5edab5b3e78
Yash Chauhan <Yash.chauhan@mechlintech.com>
2024-10-25 19:43:32 +0530

    learning  things in the drf

--- 12DRF/api/urls.py
+++ 12DRF/api/urls.py
@@ -1,7 +1,15 @@
 from home.views import peoples
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from home.views import PersonViewSet
+
+router = DefaultRouter()
+router.register(r'person_viewset_data', PersonViewSet, basename="person_viewset_data")
+
 
 urlpatterns = [
-    path('peoples/',peoples),
-    path('peoples/<int:id>',peoples,name="people_details")
+       path("", include(router.urls)),
+    path("peoples/", peoples),
+    path("peoples/<int:id>", peoples, name="people_details"),
+ 
 ]
--- 12DRF/home/views.py
+++ 12DRF/home/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets,status
 from .models import Person
 from home.serializers import PeopleSerializer
 from django.shortcuts import get_object_or_404
@@ -50,3 +50,24 @@ def peoples(request,id=None):
             return Response({'error': 'ID not provided'}, status=status.HTTP_400_BAD_REQUEST)
 
     return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset=Person.objects.all
+    serializer_class=PeopleSerializer
+
+    def get_queryset(self,id=None):
+        return Person.objects.all()  # Ensure it's returning a queryset, not a method or something else
+    
+    def reterive(self,request,*args,**kwargs):
+        instance=self.get_object()
+        serializer=self.get_serializer(instance)
+        return Response(serializer.data)\
+     # Optionally, override the destroy method if you want custom logic
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()  # Get the object to delete
+        self.perform_destroy(instance)  # Perform the delete operation
+        return Response(status=204)  # Return a No Content response
+    
